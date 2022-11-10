65ffcf771dd80a002c98238903c2a46ece6511f8
roudai <me@roudai.net>
2022-11-11 00:59:17 +0900

    左右余白調整

--- pages/index.vue
+++ pages/index.vue
@@ -6,11 +6,11 @@
 
     <div class="main-contents">
       <div class="columns is-variable is-1-mobile is-mobile">
-        <div class="column is-0-mobile"></div>
+        <div class="column"></div>
         <card title="Music" icon="music-box-multiple" url="https://linkco.re/uRn3hSfu?lang=ja"></card>
         <card title="Twitter" icon="twitter" url="https://twitter.com/timeleaaap"></card>
         <card title="YouTube" icon="youtube" url="https://www.youtube.com/channel/UC0uT6Qi6sYX0RLOWwKcK0LA"></card>
-        <div class="column is-0-mobile"></div>
+        <div class="column"></div>
       </div>
 
       <div class="columns is-variable is-1-mobile is-mobile">
@@ -21,11 +21,11 @@
       </div>
 
       <div class="columns is-variable is-1-mobile is-mobile">
-        <div class="column is-0-mobile"></div>
+        <div class="column"></div>
         <card title="lit.link" icon="link-plus" url="https://lit.link/httpsjiyuuujikancom"> </card>
         <card title="SHOWROOM" icon="filmstrip" url="https://www.showroom-live.com/r/827a36608516"></card>
         <card title="Base" icon="cart" url="https://jiyuuujikan.base.shop"> </card>
-        <div class="column is-0-mobile"></div>
+        <div class="column"></div>
       </div>
 
       <div class="columns is-hidden-touch is-centered">
