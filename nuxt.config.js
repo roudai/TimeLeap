5219ecdaa776aac5c649e89dc2b9e6ecf29842f0
roudai <me@roudai.net>
2022-11-14 11:41:29 +0900

    title変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'time-leap',
+    title: 'タイムリープ公式',
     htmlAttrs: {
       lang: 'ja',
     },
