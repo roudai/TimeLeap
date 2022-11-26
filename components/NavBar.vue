e0c5fff26087508408300263f29b4e739ddce2d2
roudai <me@roudai.net>
2022-11-26 17:32:09 +0900

    ロゴ変更

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar has-background-white-bis" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <a class="navbar-item" href="/">
-        <img src="~assets/logo.webp" alt="タイムリープ" height="50" />
+        <img src="~assets/logo.png" alt="タイムリープ" height="50" />
       </a>
     </div>
   </nav>
